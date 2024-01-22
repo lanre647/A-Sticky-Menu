e8aa994166d5f393d51998e257b5a38a923bfad5
Olanrewaju_Williams <owolabilanre2006@gmail.com>
2024-01-22 09:26:24 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let navbar = document.querySelector("#navbar");
+let menu = document.querySelector("#menu");
+
+window.onscroll = function () {
+  if (window.pageYOffset >= menu.offsetTop) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+};
